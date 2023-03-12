42f21f5d1342cf0d93a7f31e7fb6209b7dd73f98
luotl <3148623140@qq.com>
2023-03-12 14:05:39 +0800

    02_src_初始setup

---
+++ 02_src_初始setup/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h2>姓名：{{name}}</h2>
+  <h2>年龄：{{age}}</h2>
+  <button @click="sayHello">hello</button>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    //此处只是测试一下setup，暂时不考虑响应式的问题。
+    setup() {
+      let name = "Vue"
+      let age = 18
+      function sayHello() {
+        alert('你好')
+      }
+      return {
+        name,
+        age,
+        sayHello
+      }
+    }
+  }
+</script>
