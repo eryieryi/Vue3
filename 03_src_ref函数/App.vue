427cd984279e35b26a72e80607ae1218a5b25cef
luotl <3148623140@qq.com>
2023-03-12 14:49:30 +0800

    03_src_ref函数

---
+++ 03_src_ref函数/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <h2>姓名：{{name}}</h2>
+  <h2>年龄：{{age}}</h2>
+  <h3>工作：{{job.type}}</h3>
+  <h3>薪资：{{job.salary}}</h3>
+  <button @click="changeInfo">点击修改数据</button>
+</template>
+
+<script>
+  import {ref} from 'vue'
+  export default {
+    name: 'App',
+    setup() {
+      let name = ref("Vue")
+      let age = ref(18)
+      let job = ref({
+        type : '前端',
+        salary : '20k'
+      })
+      function changeInfo() {
+        name.value = '李四'
+        age.value = 40
+        job.value.type = 'UI'
+        job.value.salary = '30k'
+      }
+      return {
+        name,
+        age,
+        job,
+        changeInfo
+      }
+    }
+  }
+</script>
