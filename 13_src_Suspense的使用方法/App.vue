4e324aa8db348ccc7017f976608396c334b269d1
luotl <3148623140@qq.com>
2023-03-14 12:20:02 +0800

    Suspense的使用方法

---
+++ 13_src_Suspense的使用方法/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="app">
+    <h1>App组件</h1>
+    <Suspense>
+      <template v-slot:default>
+        <Demo></Demo>
+      </template>
+      <template v-slot:fallback>
+        <div>
+          <h2>加载中...</h2>
+        </div>
+      </template>
+    </Suspense>
+  </div>
+</template>
+
+<script>
+  // import Demo from './components/Demo'// 静态引入
+  import {defineAsyncComponent} from 'vue'
+  const Demo = defineAsyncComponent(()=>import('./components/Demo.vue')) // 动态引入
+  export default {
+    name: 'App',
+    components:{Demo}
+  }
+</script>
+
+<style lang="css">
+  .app{
+    background-color: gray;
+    padding: 10px;
+  }
+</style>
---
+++ 13_src_Suspense的使用方法/components/Demo.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="demo">
+        <h2>demo组件</h2>
+        {{sum}}
+    </div>
+</template>
+
+<script>
+  import {ref} from 'vue'
+  export default {
+    name: 'App',
+    async setup() {
+        let sum = ref(0)
+        let p = new Promise((resolve,reject)=>{
+            setTimeout(() => {
+                resolve({sum})
+            }, 1000);
+        })
+        return await p
+    }
+  }
+</script>
+
+<style lang="css">
+    .demo{
+        background-color: skyblue;
+        padding: 10px;
+    }
+</style>
